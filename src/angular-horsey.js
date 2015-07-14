e6c021b60ed7282ed62246fc28439809abc84d94
dottavio <dottavio@gmail.com>
2015-07-14 13:37:33 -0300

    First commit

---
+++ src/angular-horsey.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc object
+ * @name angular-horsey
+ * @description
+ *
+ * This is the main script for this module, only init the module
+ *
+ */
+angular
+    .module('angular-horsey', []);
